cfcf0623506ec09142e9dd4bec9b69bc3a1b14e3
Ulrich Armel <ulrichsgraphixs@gmail.com>
2025-11-02 04:11:59 +0100

    added seed.py

---
+++ python-generators-0x00/seed.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+import mysql.connector
+import csv
+import uuid
+
+def connect_db():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=""      # << put your mysql root password here if you have
+        )
+        return connection
+    except Exception as e:
+        print(e)
+        return None
+
+def create_database(connection):
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS ALX_prodev;")
+    connection.commit()
+    cursor.close()
+
+def connect_to_prodev():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="123",    
+            database="ALX_prodev"
+        )
+        return connection
+    except Exception as e:
+        print(e)
+        return None
+
+def create_table(connection):
+    cursor = connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS user_data(
+            user_id VARCHAR(36) PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            age DECIMAL NOT NULL
+        );
+    """)
+    connection.commit()
+    cursor.close()
+    print("Table user_data created successfully")
+
+def insert_data(connection, csv_file):
+    cursor = connection.cursor()
+
+    with open(csv_file, "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            cursor.execute("""
+                INSERT IGNORE INTO user_data (user_id, name, email, age)
+                VALUES (%s, %s, %s, %s)
+            """, (row["user_id"], row["name"], row["email"], row["age"]))
+
+    connection.commit()
+    cursor.close()
