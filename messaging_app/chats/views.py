5401827ee81a7a66e44e0aaeb7d42e0f9a320b06
Ulrich Armel <ulrichsgraphixs@gmail.com>
2025-11-14 12:19:14 +0100

    implimented post and get for message and conversation in views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,33 +1,46 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer, ConversationCreateSerializer, MessageCreateSerializer
 from rest_framework.response import Response
 from rest_framework import status
-from django.shortcuts import get_object_or_404
-from django.urls import  render
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    def post(self, request, *args, **kwargs):
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['participants__email', 'participants__first_name']
+    ordering_fields = ['created_at']
+    ordering = ['-created_at']
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return ConversationCreateSerializer
+        return ConversationSerializer
+
+    def create(self, request, *args, **kwargs):
         serializer = ConversationCreateSerializer(data=request.data)
         if serializer.is_valid():
-            conversation = serializer.save()
+            conversation = Conversation.objects.create()
             return Response(ConversationSerializer(conversation).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    def post(self, request, *args, **kwargs):
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['sender__email', 'message_body']
+    ordering_fields = ['sent_at']
+    ordering = ['-sent_at']
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return MessageCreateSerializer
+        return MessageSerializer
+
+    def create(self, request, *args, **kwargs):
         serializer = MessageCreateSerializer(data=request.data)
         if serializer.is_valid():
             message = serializer.save(sender=request.user)
             return Response(MessageSerializer(message).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-    
