61ae89b0e7b42288479b022eefd1bf43de70f1be
Ulrich Armel <ulrichsgraphixs@gmail.com>
2025-11-14 12:23:51 +0100

    added default router

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from . import views
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', views.ConversationViewSet, basename='conversation')
 router.register(r'messages', views.MessageViewSet, basename='message')
 
