4233ee53ec0a830c4c45113c86203797de4dc77a
Ulrich Armel <armelulrich16@gmail.com>
2025-11-23 03:46:57 +0100

    Extend custom auth globally

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,29 +1,25 @@
-from rest_framework.permissions import BasePermission, IsAuthenticated
-from chats.models import Conversation, Message
+from rest_framework.permissions import BasePermission
+from .models import Conversation, Message
+from django.contrib.auth import get_user_model
+from rest_framework.exceptions import PermissionDenied
 from rest_framework import permissions
-
 
 class IsParticipantOfConversation(BasePermission):
     """
-    Allows access only to authenticated users who are part of the conversation.
+    Grants access only to authenticated users who are participants
+    of the conversation or message they are trying to access.
     """
 
     def has_permission(self, request, view):
-        # Step 1: User must be authenticated
+        # Only allow access if the user is authenticated
         return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        """
-        Object-level check.
-        - `obj` may be a Conversation or a Message.
-        - We check if the requesting user is a participant.
-        """
-
-        # If the object is a Conversation
+        # If the object is a Conversation, check if the user is a participant
         if hasattr(obj, "participants"):
             return request.user in obj.participants.all()
 
-        # If the object is a Message (belongs to a conversation)
+        # If the object is a Message, check if the user is a participant in the related conversation
         if hasattr(obj, "conversation"):
             return request.user in obj.conversation.participants.all()
 
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -87,6 +87,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',  
         'rest_framework.authentication.BasicAuthentication',    
         'rest_framework.authentication.SessionAuthentication',  
+        'chats.auth.CustomJWTAuthentication',
     ),
 
     'DEFAULT_PERMISSION_CLASSES': (
