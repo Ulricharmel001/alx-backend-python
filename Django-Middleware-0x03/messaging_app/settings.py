ae707418116417adfa651281e31d05806ea0a223
Ulrich Armel <armelulrich16@gmail.com>
2025-12-02 17:33:26 +0100

    reposition request.log

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'chats.middleware.RestrictAccessByTimeMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware'
+    
     
     
     
