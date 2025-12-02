159bbd6453dc16e69b526fcabebb647ffd54128c
Ulrich Armel <armelulrich16@gmail.com>
2025-12-02 16:27:16 +0100

    added a middlie ware that restrict access to chat and app between 9pm and 6am

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,13 +1,14 @@
 import time
 import logging
-from datetime import datetime
+from datetime import datetime, time
 from django.utils.deprecation import MiddlewareMixin
 from django.conf import settings
 from django.utils.timezone import now
 from chats.models import User
+from django.http import HttpResponseForbidden
 
 
-class RequestLoggingMiddleware():
+class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         self.logger = logging.getLogger(__name__)
@@ -18,7 +19,6 @@ class RequestLoggingMiddleware():
         duration = time.time() - start_time
 
         user = request.user if request.user.is_authenticated else None
-        # Use user_id (UUID) for custom User model instead of id
         user_info = f'User ID: {user.user_id}, Email: {user.email}' if user else 'Anonymous User'
 
         log_data = {
@@ -34,3 +34,30 @@ class RequestLoggingMiddleware():
         self.logger.info(f"Request Log: {log_data}")
         return response
 
+
+
+class  RestrictAccessByTimeMiddleware:
+    """Middleware to resrtrict access to a chat and app 
+    between 9pm and  6 Am"""
+    
+    def __init__(self, get_response):
+        self.get_response = get_response
+        
+        
+    def __call__(self, request):
+        # Restrict access between 9 PM and 6 AM
+        current_time = datetime.now().time()
+        start_restrict = time(21, 0)  # 9 PM
+        end_restrict = time(6, 0)     # 6 AM
+
+        # If current time is between 9 PM and midnight, or between midnight and 6 AM
+        if (current_time >= start_restrict or current_time < end_restrict):
+            return HttpResponseForbidden("Access to this app is restricted between 9 PM and 6 AM.")
+
+        response = self.get_response(request)
+        return response
+        
+    
+    
+    
+    
